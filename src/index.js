f828c2830dd0ed96db0650eedf5a9a9e425165aa
IfalEXZ <ifaltampan69@gmail.com>
2024-11-20 15:20:36 +0800

    rename: change package name

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { fetc } from './fetc';
-export { useFetch } from './useFetch';
-export { useCache } from './cache';
-export { retryFetch } from './utils';
+export { fetcho } from "./fetcho";
+export { useFetch } from "./useFetch";
+export { useCache } from "./cache";
+export { retryFetch } from "./utils";
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { fetc } from './fetc'; 
+import { useState, useEffect } from "react";
+import { fetcho } from "./fetcho";
 
 /**
  * Custom React hook for fetching data.
@@ -16,7 +16,7 @@ export const useFetch = (url, options = {}) => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const result = await fetc(url, options);  // Use fetchs function
+        const result = await fetcho(url, options); // Use fetchs function
         setData(result);
       } catch (err) {
         setError(err.message);
